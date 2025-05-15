6944d19d5cb2ec096adb235369994b0c34821064
stephanflug <48253885+stephanflug@users.noreply.github.com>
2025-05-15 08:59:23 +0200

    sdcheck.sh

--- addons/sdcheck.sh
+++ addons/sdcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Header für Server-Sent-Events
+# Server-Sent Events Header (für CGI/Webserver wie lighttpd)
 echo "Content-Type: text/event-stream"
 echo "Cache-Control: no-cache"
 echo "Connection: keep-alive"
@@ -9,16 +9,20 @@ echo ""
 echo "data: Starte SD-Karten-Fehlerüberprüfung..."
 echo ""
 
-# Prüfe dmesg auf SD-Karten- oder I/O-Fehler
-ERRORS=$(dmesg | grep -iE 'mmc|error|fail|io error')
+# Hole relevante dmesg-Zeilen
+LOG=$(dmesg | grep -iE 'mmc|error|fail|io error')
 
-if [ -n "$ERRORS" ]; then
-    echo "data: SD-Karten Fehler entdeckt:"
-    echo "$ERRORS" | while IFS= read -r line; do
+# Prüfe auf echte Fehler (nicht nur SD-Initialisierungen)
+FEHLER=$(echo "$LOG" | grep -iE 'error|fail|io error')
+
+if [ -n "$FEHLER" ]; then
+    echo "data:SD-Kartenfehler entdeckt:"
+    echo ""
+    echo "$FEHLER" | while IFS= read -r line; do
         echo "data: $line"
     done
 else
-    echo "data: Keine SD-Karten-Fehler in dmesg gefunden."
+    echo "data:Keine SD-Karten-Fehler in dmesg gefunden."
 fi
 
 echo ""
